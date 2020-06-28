deba8cdb0ea94267f770f259aab61ed933647bb5
Chaturvedi <sulchaturvedi@lm-lhr-11355355.corp.ebay.com>
2020-06-28 18:41:19 +0100

    inital commit

--- main.go
+++ main.go
@@ -9,10 +9,11 @@ import (
 func main() {
 	host := flag.String("host", "scanme.nmap.org", "FQDN of the host to scan")
 	port := flag.String("port", "80", "port to scan")
-	addr := *host + *port
+	addr := *host + ":" + *port
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
+	fmt.Println("Connection succeeded")
 	defer conn.Close()
 }
