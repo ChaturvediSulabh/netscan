bd1988ef39032c53e00dc2273fb77ff84f46358f
Chaturvedi <sulchaturvedi@lm-lhr-11355355.corp.ebay.com>
2020-06-29 22:15:09 +0100

    scan a range of ports

--- main.go
+++ main.go
@@ -4,16 +4,24 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"strconv"
 )
 
 func main() {
 	host := flag.String("host", "scanme.nmap.org", "FQDN of the host to scan")
-	port := flag.String("port", "80", "port to scan")
-	addr := *host + ":" + *port
-	conn, err := net.Dial("tcp", addr)
-	if err != nil {
-		panic(err)
+	portMin := flag.Int("portMin", 1, "start scanning from this port")
+	portMax := flag.Int("portMax", 1024, "port to scan")
+
+	flag.Parse()
+
+	for i := *portMin; i <= *portMax; i++ {
+		addr := *host + ":" + strconv.Itoa(i)
+		conn, err := net.Dial("tcp", addr)
+		if err != nil {
+			fmt.Printf("Connection refused for %v on %v\n", *host, i)
+			continue
+		}
+		fmt.Printf("Connection succeeded for %v on %v\n", *host, i)
+		defer conn.Close()
 	}
-	fmt.Println("Connection succeeded")
-	defer conn.Close()
 }
